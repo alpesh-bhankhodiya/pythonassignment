308d84c9e490523d38ca27127ea5966258538ea6
alpesh-bhankhodiya <alpeshbhankhodiya@gmail.com>
2026-01-02 21:30:17 +0530

    Add files via upload

---
+++ Task 1.py
@@ -0,0 +1,10 @@
+a = int(input("Enter a number:"))
+b = int(input("Enter a number:"))
+
+print("Enter the first number :",a)
+print("Enter the second number :",b)
+
+print("Addition :",a+b)
+print("Subtraction :",a-b)
+print("multiplication :",a*b)
+print("Division :",a/b)
---
+++ Task 2.py
@@ -0,0 +1,7 @@
+first_name = input("Enter your first name :")
+last_name = input("Enter your last name :")
+
+print("Enter your first name :", first_name)
+print("Enter your last name :", last_name)
+
+print("Hell0,",first_name,last_name,"Welcome to the python Program.")
